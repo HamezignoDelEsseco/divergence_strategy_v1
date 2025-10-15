6d68edda92d89508cf25af31a25055715e71a1c8
RB <rb.mini.win@gmail.com>
2025-10-15 16:40:48 +0200

    cleaned compression signal with intermediate outputs

--- ExtensionCompression.cpp
+++ ExtensionCompression.cpp
@@ -29,7 +29,7 @@ void updateNBars(const double high, const double low, const float fh, const floa
         aboveFL += 1;
     if (high <= fl)
         belowFL += 1;
-};
+}
 
 
 template<typename... Subgraphs>
@@ -43,29 +43,13 @@ SCSFExport scsf_CompressionExtensionSignal(SCStudyInterfaceRef sc) {
     SCInputRef StudyLines = sc.Input[0];
     SCInputRef MinNumberOfExtensionCompressionTicks = sc.Input[1];
     SCInputRef TicksForJump = sc.Input[2];
-    SCInputRef TicksForRetrace = sc.Input[3];
-    SCInputRef ResetOnJump = sc.Input[4];
+    SCInputRef ResetOnJump = sc.Input[3];
 
-    SCSubgraphRef ExtensionFlag = sc.Subgraph[0];
-    SCSubgraphRef ExtensionStartIndex = sc.Subgraph[1];
-    SCSubgraphRef ExtensionSymmetricPriceFH = sc.Subgraph[2];
-    SCSubgraphRef ExtensionSymmetricPriceFL = sc.Subgraph[3];
+    SCSubgraphRef ExtensionID = sc.Subgraph[0];
+    SCSubgraphRef ExtensionSymmetricPriceFH = sc.Subgraph[1];
+    SCSubgraphRef ExtensionSymmetricPriceFL = sc.Subgraph[2];
 
-    SCSubgraphRef CompressionFlag = sc.Subgraph[4];
-    SCSubgraphRef CompressionStartIndex = sc.Subgraph[5];
-    SCSubgraphRef CompressionSymmetricPriceFH = sc.Subgraph[6];
-    SCSubgraphRef CompressionSymmetricPriceFL = sc.Subgraph[7];
-
-    SCSubgraphRef Midpoint = sc.Subgraph[8];
-
-    SCSubgraphRef FVDiff = sc.Subgraph[9];
-    SCSubgraphRef FVDiffIncrement = sc.Subgraph[10];
-    SCSubgraphRef CumulativeFVDiffPos = sc.Subgraph[11];
-    SCSubgraphRef CumulativeFVDiffNeg = sc.Subgraph[12];
-
-
-    SCSubgraphRef CumulativeFVRetraceTicks = sc.Subgraph[13];
-    SCSubgraphRef CurrentStatus = sc.Subgraph[14];
+    SCSubgraphRef CurrentStatus = sc.Subgraph[3];
 
 
 
@@ -86,51 +70,29 @@ SCSFExport scsf_CompressionExtensionSignal(SCStudyInterfaceRef sc) {
         TicksForJump.SetIntLimits(5,100);
         TicksForJump.SetInt(10);
 
-        TicksForRetrace.Name = "Ticks for retrace";
-        TicksForRetrace.SetIntLimits(1,100);
-        TicksForRetrace.SetInt(1);
-
         ResetOnJump.Name = "Reset extension compression on jump";
         ResetOnJump.SetYesNo(1);
 
         // Outputs
-        ExtensionFlag.Name = "Extension flag";
-        ExtensionFlag.DrawStyle = DRAWSTYLE_IGNORE;
-        ExtensionStartIndex.Name = "Extension start index";
-        ExtensionStartIndex.DrawStyle = DRAWSTYLE_IGNORE;
+        ExtensionID.Name = "Extension start index";
+        ExtensionID.DrawStyle = DRAWSTYLE_IGNORE;
         ExtensionSymmetricPriceFH.Name = "Extension sym price high";
-        ExtensionSymmetricPriceFH.DrawStyle = DRAWSTYLE_IGNORE;
+        ExtensionSymmetricPriceFH.DrawStyle = DRAWSTYLE_LINE;
         ExtensionSymmetricPriceFL.Name = "Extension sym price low";
-        ExtensionSymmetricPriceFL.DrawStyle = DRAWSTYLE_IGNORE;
-        CompressionFlag.Name = "Compression flag";
-        CompressionFlag.DrawStyle = DRAWSTYLE_IGNORE;
-        CompressionStartIndex.Name = "Compression start index";
-        CompressionStartIndex.DrawStyle = DRAWSTYLE_IGNORE;
-        CompressionSymmetricPriceFH.Name = "Compression sum price high";
-        CompressionSymmetricPriceFH.DrawStyle = DRAWSTYLE_IGNORE;
-        CompressionSymmetricPriceFL.Name = "Compression sum price low";
-        CompressionSymmetricPriceFL.DrawStyle = DRAWSTYLE_IGNORE;
+        ExtensionSymmetricPriceFL.DrawStyle = DRAWSTYLE_LINE;
 
-        FVDiff.Name = "FV diff";
-        FVDiff.DrawStyle = DRAWSTYLE_IGNORE;
-        FVDiffIncrement.Name = "FV Diff increment";
-        FVDiffIncrement.DrawStyle = DRAWSTYLE_IGNORE;
+        // FVDiff.Name = "FV diff";
+        // FVDiff.DrawStyle = DRAWSTYLE_IGNORE;
+        // FVDiffIncrement.Name = "FV Diff increment";
+        // FVDiffIncrement.DrawStyle = DRAWSTYLE_IGNORE;
+//
+        // CumulativeFVDiffPos.Name = "Cumulative FV diff positive";
+        // CumulativeFVDiffPos.DrawStyle = DRAWSTYLE_IGNORE;
+        // CumulativeFVDiffNeg.Name = "Cumulative FV diff negative";
+        // CumulativeFVDiffNeg.DrawStyle = DRAWSTYLE_IGNORE;
 
-        //CumulativeFHIncrementTicks.DrawStyle = DRAWSTYLE_IGNORE;
-        //CumulativeFLIncrementTicks.DrawStyle = DRAWSTYLE_IGNORE;
-        CumulativeFVDiffPos.Name = "Cumulative FV diff positive";
-        CumulativeFVDiffPos.DrawStyle = DRAWSTYLE_IGNORE;
-        CumulativeFVDiffNeg.Name = "Cumulative FV diff negative";
-        CumulativeFVDiffNeg.DrawStyle = DRAWSTYLE_IGNORE;
-        //CumulativeFVIncrementTicks.DrawStyle = DRAWSTYLE_IGNORE;
-
-        CumulativeFVRetraceTicks.Name = "Cumulative FV retrace ticks";
-        CumulativeFVRetraceTicks.DrawStyle = DRAWSTYLE_IGNORE;
         CurrentStatus.Name = "Current status";
         CurrentStatus.DrawStyle = DRAWSTYLE_IGNORE;
-
-        Midpoint.Name = "Midpoint";
-        Midpoint.DrawStyle = DRAWSTYLE_LINE;
     }
     const int i = sc.Index;
     SCString Buffer;
@@ -143,11 +105,31 @@ SCSFExport scsf_CompressionExtensionSignal(SCStudyInterfaceRef sc) {
     sc.GetStudyArrayUsingID(StudyLines.GetStudyID(), 1, FH);
     sc.GetStudyArrayUsingID(StudyLines.GetStudyID(), 2, FL);
 
+    // Intermediate results used for plotting the symmetric extensions / compressions
+    SCFloatArray& FVHDiff = ExtensionSymmetricPriceFH.Arrays[0];
+    SCFloatArray& FVHDiffCum = ExtensionSymmetricPriceFH.Arrays[1];
+    SCFloatArray& FVLDiff = ExtensionSymmetricPriceFL.Arrays[0];
+    SCFloatArray& FVLDiffCum = ExtensionSymmetricPriceFL.Arrays[1];
+
+    // Intermediate results for computing the compression / extension zones
+    SCFloatArray& FVDiff = CurrentStatus.Arrays[0];
+    SCFloatArray& FVDiffIncrement = CurrentStatus.Arrays[1];
+    SCFloatArray& CumulativeFVDiffNeg = CurrentStatus.Arrays[3];
+    SCFloatArray& CumulativeFVDiffPos = CurrentStatus.Arrays[4];
+
+
     float currentType;
 
-    // FVDiff[i] = FVDiff[i-1] - (FL[i] - FL[i-1]) / sc.TickSize;
     FVDiff[i] = (FH[i] - FL[i]) / sc.TickSize;
     FVDiffIncrement[i] = FVDiff[i] - FVDiff[i-1];
+
+
+    FVHDiff[i] = FH[i] - FH[i-1];
+    FVHDiffCum[i] = FVHDiff[i] + FVHDiffCum[i-1];
+    FVLDiff[i] = FL[i] - FL[i-1];
+    FVLDiffCum[i] = FVLDiff[i] + FVLDiffCum[i-1];
+
+
 
     if(FVDiffIncrement[i] == 0) {
         CumulativeFVDiffNeg[i] = CumulativeFVDiffNeg[i-1];
@@ -160,34 +142,38 @@ SCSFExport scsf_CompressionExtensionSignal(SCStudyInterfaceRef sc) {
         CumulativeFVDiffPos[i] = CumulativeFVDiffPos[i-1] + FVDiffIncrement[i];
      }
 
-    // CumulativeFVDiffPos[i] = CumulativeFVDiffPos[i-1] + FVDiffIncrement[i];
-    // CumulativeFVDiff[i] = CumulativeFHIncrementTicks[i] + FVDiff[i];
 
-    if (CumulativeFVDiffPos[i] >= MinNumberOfExtensionCompressionTicks.GetFloat() && CurrentStatus[i-1] != 1) {
+    // Regime change logic
+    if (CumulativeFVDiffPos[i] >= MinNumberOfExtensionCompressionTicks.GetFloat() && CurrentStatus[i-1] != 1) { // Switch to Extension
         currentType = 1;
         CumulativeFVDiffPos[i] = 0;
-        ResetSubgraphsAtIndex(i, CumulativeFVDiffPos, FVDiffIncrement); // DO NOT RESET FVDiff !!!
-    } else if (CumulativeFVDiffNeg[i] <= -MinNumberOfExtensionCompressionTicks.GetFloat() && CurrentStatus[i-1] != 2) {
+        ResetSubgraphsAtIndex(i, CumulativeFVDiffPos, FVDiffIncrement, FVHDiffCum, FVLDiffCum); // DO NOT RESET FVDiff !!!
+        //FVHDiffCum[i] = 0;
+        //FVLDiffCum[i] = 0;
+        ExtensionID[i] = ExtensionID[i-1] + 1;
+
+    } else if (CumulativeFVDiffNeg[i] <= -MinNumberOfExtensionCompressionTicks.GetFloat() && CurrentStatus[i-1] != 2) { // Switch to Compression
         currentType = 2;
         CumulativeFVDiffPos[i] = 0;
-        ResetSubgraphsAtIndex(i, CumulativeFVDiffPos, FVDiffIncrement); // DO NOT RESET FVDiff !!!
+        ResetSubgraphsAtIndex(i, CumulativeFVDiffPos, FVDiffIncrement, FVHDiffCum, FVLDiffCum); // DO NOT RESET FVDiff !!!
+        // FVHDiffCum[i] = 0;
+        // FVLDiffCum[i] = 0;
+        ExtensionID[i] = ExtensionID[i-1] + 1;
 
     } else {
         currentType = CurrentStatus[i-1];
+        ExtensionID[i] = ExtensionID[i-1];
     }
 
 
-    // Reset on new day or Beginning of chart history
-
+    // Reset on new day or beginning of chart
     if (sc.IsNewTradingDay(i) || i <= 0) {
         CumulativeFVDiffPos[i] = 0;
         CumulativeFVDiffNeg[i] = 0;
         FVDiffIncrement[i] = 0;
-        //FVDiff[i] = 0;
-        //ResetSubgraphsAtIndex(
-        //    i, ExtensionFlag, ExtensionStartIndex, ExtensionSymmetricPriceFH, ExtensionSymmetricPriceFL,
-        //    CompressionFlag, CompressionStartIndex, CompressionSymmetricPriceFH, CompressionSymmetricPriceFL,
-        //    CumulativeFVDiff, FVDiffIncrement, FVDiff, CumulativeFVRetraceTicks);
+        FVHDiffCum[i] = 0;
+        FVLDiffCum[i] = 0;
+        ExtensionID[i] = 0;
         currentType = 0;
     }
 
@@ -196,12 +182,11 @@ SCSFExport scsf_CompressionExtensionSignal(SCStudyInterfaceRef sc) {
         CumulativeFVDiffPos[i] = 0;
         FVDiffIncrement[i] = 0;
         CumulativeFVDiffNeg[i] = 0;
-//
-        //FVDiff[i] = 0;
-//
         currentType = 0;
     }
 
     CurrentStatus[i] = currentType;
-    Midpoint[i] = (FH[i] + FL[i]) / 2;
+    ExtensionSymmetricPriceFH[i] = FH[i] - FVLDiffCum[i];
+    ExtensionSymmetricPriceFL[i] = FL[i] - FVHDiffCum[i];
+
 }
